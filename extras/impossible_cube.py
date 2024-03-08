da32d703b83ed6fcaba1e925c97430e0aa39e425
Rashad Aziz <rashad.aziz@ui.ac.id>
2024-03-08 17:05:33 +1000

    misc: added stencil offset paramater to ImpossibleCube object

--- extras/impossible_cube.py
+++ extras/impossible_cube.py
@@ -8,8 +8,10 @@ from math import pi
 
 
 class ImpossibleCube(Object3D):
-    def __init__(self, object_material=SurfaceMaterial) -> None:
+    def __init__(self, object_material=SurfaceMaterial, stencil_ref_offset=0) -> None:
         super().__init__()
+
+        ref = stencil_ref_offset
 
         face_geom = RectangleGeometry(width=2, height=2)
         face_properties = {"stencilWrite": True, "baseColor": [1, 1, 1],
@@ -71,67 +73,67 @@ class ImpossibleCube(Object3D):
             border4.rotate_z(pi/2)
 
         face1 = Mesh(face_geom, SurfaceMaterial(
-            {"stencilRef": 1, **face_properties}))
+            {"stencilRef": ref+1, **face_properties}))
         object1 = Mesh(SphereGeometry(radius=0.5), object_material(
-            properties={"baseColor": [1.0, 0.0, 0.0], "stencilRef": 1, **object_properties}))
+            properties={"baseColor": [1.0, 0.0, 0.0], "stencilRef": ref+1, **object_properties}))
         face1.translate(0, 0, 1)
         object1.translate(0, 0, -1)
         face1.add(object1)
         self.add(face1)
-        generate_walls(face1, 1)
+        generate_walls(face1, ref+1)
 
         face2 = Mesh(face_geom, SurfaceMaterial(
-            {"stencilRef": 2, **face_properties}))
+            {"stencilRef": ref+2, **face_properties}))
         object2 = Mesh(BoxGeometry(), object_material(
-            properties={"baseColor": [0, 0, 1], "stencilRef": 2, **object_properties}))
+            properties={"baseColor": [0, 0, 1], "stencilRef": ref+2, **object_properties}))
         face2.translate(1, 0, 0)
         face2.rotate_y(pi/2)
         object2.translate(0, 0, -1)
         face2.add(object2)
         self.add(face2)
-        generate_walls(face2, 2)
+        generate_walls(face2, ref+2)
 
         face3 = Mesh(face_geom, SurfaceMaterial(
-            {"stencilRef": 3, **face_properties}))
+            {"stencilRef": ref+3, **face_properties}))
         object3 = Mesh(PyramidGeometry(width_bottom=0.75), object_material(
-            properties={"baseColor": [1, 0, 1], "stencilRef": 3, **object_properties}))
+            properties={"baseColor": [1, 0, 1], "stencilRef": ref+3, **object_properties}))
         face3.translate(0, 0, -1)
         face3.rotate_y(pi)
         object3.translate(0, 0, -1)
         face3.add(object3)
         self.add(face3)
-        generate_walls(face3, 3)
+        generate_walls(face3, ref+3)
 
         face4 = Mesh(face_geom, SurfaceMaterial(
-            {"stencilRef": 4, **face_properties}))
+            {"stencilRef": ref+4, **face_properties}))
         object4 = Mesh(CylinderGeometry(radius=0.5), object_material(
-            properties={"baseColor": [1, 1, 0], "stencilRef": 4, **object_properties}))
+            properties={"baseColor": [1, 1, 0], "stencilRef": ref+4, **object_properties}))
         face4.translate(-1, 0, 0)
         face4.rotate_y(-pi/2)
         object4.translate(0, 0, -1)
         face4.add(object4)
         self.add(face4)
-        generate_walls(face4, 4)
+        generate_walls(face4, ref+4)
 
         face5 = Mesh(face_geom, SurfaceMaterial(
-            {"stencilRef": 5, **face_properties}))
+            {"stencilRef": ref+5, **face_properties}))
         object5 = Mesh(EllipsoidGeometry(width=1.25), object_material(
-            properties={"baseColor": [0.5, 0.2, 1], "stencilRef": 5, **object_properties}))
+            properties={"baseColor": [0.5, 0.2, 1], "stencilRef": ref+5, **object_properties}))
         face5.translate(0, 1, 0)
         face5.rotate_x(-pi/2)
         object5.translate(0, 0, -1)
         face5.add(object5)
         self.add(face5)
-        generate_walls(face5, 5)
+        generate_walls(face5, ref+5)
 
         face6 = Mesh(face_geom, SurfaceMaterial(
-            {"stencilRef": 6, **face_properties}))
+            {"stencilRef": ref+6, **face_properties}))
         object6 = Mesh(PrismGeometry(radius=0.5), object_material(
-            properties={"baseColor": [0.2, 1, 0.5], "stencilRef": 6, **object_properties}))
+            properties={"baseColor": [0.2, 1, 0.5], "stencilRef": ref+6, **object_properties}))
         face6.translate(0, -1, 0)
         face6.rotate_x(pi/2)
         object6.translate(0, 0, -1)
         object6.rotate_x(pi/2)
         face6.add(object6)
         self.add(face6)
-        generate_walls(face6, 6)
+        generate_walls(face6, ref+6)
